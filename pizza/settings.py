21c6e4fcdf8ae26ed505fd5bec1475172244b767
HashirGJ8842 <hameedhashirniazi@gmail.com>
2020-05-22 16:50:36 +0530

    Connected with live database

--- pizza/settings.py
+++ pizza/settings.py
@@ -77,14 +77,9 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -130,4 +125,4 @@ import django_heroku
 # This should already be in your settings.py
 django_heroku.settings(locals())
 # This is new
-#del DATABASES['default']['OPTIONS']['sslmode']
+del DATABASES['default']['OPTIONS']['sslmode']
